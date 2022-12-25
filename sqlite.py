6a3a72b650a18c448427b86d04c750014c35158f
Beksul <dddfffyyy.bk@gmail.com>
2022-12-25 17:26:06 +0600

    работает норм, только в from_user

--- sqlite.py
+++ sqlite.py
@@ -5,20 +5,26 @@ async def db_start():
     db = sq.connect("main.db")
     cur = db.cursor()
 
-    cur.execute("CREATE TABLE IF NOT EXISTS profile(user_id TEXT PRIMARY KEY, name TEXT, count INTEGER)")
+    cur.execute("CREATE TABLE IF NOT EXISTS profile(user_id TEXT PRIMARY KEY, name TEXT, count INTEGER, chance INTEGER)")
 
     db.commit()
 
 async def create_profile(user_id):
     user = cur.execute("SELECT 1 FROM profile WHERE user_id == '{key}'".format(key=user_id)).fetchone()
     if not user:
-        cur.execute("INSERT INTO profile VALUES(?, ?, ?)", (user_id, '', ''))
+        cur.execute("INSERT INTO profile VALUES(?, ?, ?, ?)", (user_id, '', '', 0))
         db.commit()
 
 async def edit_profile(state, user_id):
     async with state.proxy() as data:
         cur.execute("UPDATE profile SET  name = '{}' WHERE user_id == '{}'".format(
             data['name'], user_id))
+        db.commit()
+
+async def edit_chance(state, user_id):
+    async with state.proxy() as data:
+        cur.execute("UPDATE profile SET  chance = '{}' WHERE user_id == '{}'".format(
+            data['chance'], user_id))
         db.commit()
 
 async def new_count(newcount,id) -> None:
@@ -30,7 +36,17 @@ async def name_from_db(id):
 async def count_from_db(id):
     count = cur.execute("SElECT count FROM profile WHERE user_id = ?", (id, )).fetchone()
     return count
+async def chance_from_db(id):
+    chance = cur.execute("SElECT chance FROM profile WHERE user_id = ?", (id, )).fetchone()
+    return chance
+async def chance_set(id):
+    newchance = cur.execute("UPDATE profile SET chance = ? WHERE user_id = ?", (1, id,)).fetchone()
+    return newchance
 
 async def get_all():
     all = cur.execute("SELECT * FROM profile").fetchall()
+    return all
+
+async def get_all_user(id):
+    all = cur.execute("SELECT * FROM profile WHERE user_id = ?", (id,)).fetchall()
     return all
