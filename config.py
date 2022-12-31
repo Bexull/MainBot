da532fafdf872f5726258e3b616c58abaa14d9b1
Beksul <dddfffyyy.bk@gmail.com>
2022-12-31 22:13:48 +0600

    /gift

--- config.py
+++ config.py
@@ -1,7 +1,3 @@
-import os
-from dotenv import load_dotenv, find_dotenv
-load_dotenv(find_dotenv())
-
 TOKEN_API = '5452261343:AAGg0oo9vRZVpyPz6LUMBkyRHlNZ2grJw40'
-ADMIN_ID_1 = 1015079692
-ADMIN_ID_2 = 1087968824
+ADMIN_ID_1 = '1015079692'
+ADMIN_ID_2 = '1087968824'
